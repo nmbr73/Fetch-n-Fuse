064274b18f9477d9df418ac240021777bc116867
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2021-12-23 21:35:45 +0100

    Made it "tinySlider" with min/max/default.

--- Conversions/SimpleDCTL.TEST.c
+++ Conversions/SimpleDCTL.TEST.c
@@ -9,11 +9,11 @@ __KERNEL__ void SimpleDCTLKernel(
 
   PROLOGUE(fragColor,fragCoord);
   USE_CTRL_COLOR3(farbenspiel);
-  USE_CTRL_SLIDER2(zumausprobieren);
+  USE_CTRL_TINYSLIDER2(zumausprobieren,0.0f,1.0f,0.5f); // Name der Variablen, min, max, default
 
   float red          = farbenspiel.x;
   float green        = farbenspiel.y;
-  float blue         = zumausprobieren / 100.0f;
+  float blue         = zumausprobieren;
   float alpha        = farbenspiel.w;
 
   fragColor=to_float4(red,green,blue,alpha);
