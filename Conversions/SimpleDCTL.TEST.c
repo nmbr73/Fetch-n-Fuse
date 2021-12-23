1b64e3c7d4f9472949c9ffc1305dcd4a8f605974
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2021-12-23 21:42:40 +0100

    Some explanation.

--- Conversions/SimpleDCTL.TEST.c
+++ Conversions/SimpleDCTL.TEST.c
@@ -7,14 +7,14 @@ __KERNEL__ void SimpleDCTLKernel(
     )
 {
 
-  PROLOGUE(fragColor,fragCoord);
-  USE_CTRL_COLOR3(farbenspiel);
-  USE_CTRL_TINYSLIDER2(zumausprobieren,0.0f,1.0f,0.5f); // Name der Variablen, min, max, default
+  PROLOGUE(fragColor,fragCoord); // Standard Shader-Controls bereitstellen (iTime, iMouse und so)
+  USE_CTRL_COLOR0(Farbenspiel); // Name der 'float4' Variable - wird nachher fuer das Control in der Fuse verwendet
+  USE_CTRL_TINYSLIDER0(Ich_mach_Blau,0.0f,1.0f,0.5f); // Name der 'float' Variable, Min, Max, und Default-Wert (Default wird hier nicht, aber spaeter in der Fuse verwendet)
 
-  float red          = farbenspiel.x;
-  float green        = farbenspiel.y;
-  float blue         = zumausprobieren;
-  float alpha        = farbenspiel.w;
+  float red          = Farbenspiel.x;
+  float green        = Farbenspiel.y;
+  float blue         = Ich_mach_Blau;
+  float alpha        = Farbenspiel.w;
 
   fragColor=to_float4(red,green,blue,alpha);
 
