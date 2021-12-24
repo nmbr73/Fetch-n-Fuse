6365f359db4126e2c149eae656c22597d388b3ed
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2021-12-24 01:36:01 +0100

    Fixed the indices.

--- Conversions/SimpleDCTL.TEST.c
+++ Conversions/SimpleDCTL.TEST.c
@@ -10,11 +10,12 @@ __KERNEL__ void SimpleDCTLKernel(
   PROLOGUE(fragColor,fragCoord); // Standard Shader-Controls bereitstellen (iTime, iMouse und so)
   USE_CTRL_COLOR0(Farbenspiel); // Name der 'float4' Variable - wird nachher fuer das Control in der Fuse verwendet
   USE_CTRL_TINYSLIDER0(Ich_mach_Blau,0.0f,1.0f,0.5f); // Name der 'float' Variable, Min, Max, und Default-Wert (Default wird hier nicht, aber spaeter in der Fuse verwendet)
+  USE_CTRL_TINYSLIDER1(slider,0.0f,1.0f,0.5f);
 
-  float red          = Farbenspiel.x;
-  float green        = Farbenspiel.y;
-  float blue         = Ich_mach_Blau;
-  float alpha        = Farbenspiel.w;
+  float red          = iMouse.x/iResolution.x; // rot klebe ich mal an die Maus
+  float green        = Farbenspiel.y; // gruen haengt am Gruen des Color0-Sliders
+  float blue         = Ich_mach_Blau; // blue haengt am 0ten TinySlider
+  float alpha        = slider; // alpha haengt am TinySlider 1
 
   fragColor=to_float4(red,green,blue,alpha);
 
