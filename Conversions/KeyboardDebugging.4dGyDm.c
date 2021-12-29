e0dda5c7f7433a19f1a5f12d14c33bd2a2bf8c99
J-i-P-i <78935215+J-i-P-i@users.noreply.github.com>
2021-12-29 22:57:57 +0100

    KeyboardDebugging compilierfähig

--- Conversions/KeyboardDebugging.4dGyDm.c
+++ Conversions/KeyboardDebugging.4dGyDm.c
@@ -8,7 +8,7 @@
 
 // just debugging keyboard texture and scan codes!
 
-
+__DEVICE__ float2 abs_f2(float2 a) {return (to_float2(_fabs(a.x), _fabs(a.y)));}
 
 
 __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2 iResolution, sampler2D iChannel0, sampler2D iChannel1)
@@ -28,7 +28,7 @@ __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2
 
     float2 p = (fragCoord - 0.5f - 0.5f*iResolution)*scl + 0.5f*TABLE_DIMS;
 
-    float2 b = _fabs(p - 0.5f*TABLE_DIMS) - 0.5f*TABLE_DIMS;
+    float2 b = abs_f2(p - 0.5f*TABLE_DIMS) - 0.5f*TABLE_DIMS;
     float dbox = _fmaxf(b.x, b.y);
 
     if (dbox < 0.0f) {
@@ -39,10 +39,10 @@ __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2
 
         bool hit = false;
 
-        bool3 ktex; // bvec3 ktex;
+        bool ktex[3]; // bvec3 ktex;
         for (int i=0; i<3; ++i) {
             // https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/texelFetch.xhtml
-            ktex[i] = texelFetch(iChannel1, to_int2(keycode, i), 0).x > 0.0f;
+            ktex[i] = _tex2DVecN(iChannel1, ((float)keycode+0.5f)/iResolution.x,((float)keycode+0.5f)/iResolution.y, 15).x > 0.0f;
         }
 
         color = ktex[0] ? to_float3(1.0f, 0.25f, 0.25f) : to_float3_s(0.8f);
@@ -64,28 +64,23 @@ __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2
 
                 const float font_size = 1.0f;
 
-                float2 p0 = (cell + to_float2(0.5f + (float(i)-i0)*0.3f, 0.5f))*CELL_DIMS;
+                float2 p0 = (cell + to_float2(0.5f + ((float)(i)-i0)*0.3f, 0.5f))*CELL_DIMS;
 
                 float2 uv;
 
                 { // font_from_screen
 
                   float2 tpos = p-p0;
-                  float2 char_pos=to_float2(digit, 12)
+                  float2 char_pos=to_float2(digit, 12);
 
                   const float GLYPHS_PER_UV = 16.0f;
                   uv = (tpos/font_size + char_pos + 0.5f)/GLYPHS_PER_UV;
-
-
                 }
 
-                float2 dbox = _fabs(p - p0) - 0.5f;
-
+                float2 dbox = abs_f2(p - p0) - 0.5f;
                 { // sample_grad_dist
-
-
                   // https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/textureLod.xhtml
-                  float3 grad_dist = (textureLod(iChannel0, uv, 0.0f).yzw - FONT_TEX_BIAS) * font_size;
+                  float3 grad_dist = (swi3(_tex2DVecN(iChannel0, uv.x,uv.y, 15),y,z,w) - FONT_TEX_BIAS) * font_size;
 
                   grad_dist.y = -grad_dist.y;
                   swi2(grad_dist,x,y) = normalize(swi2(grad_dist,x,y) + 1e-5);
@@ -93,14 +88,8 @@ __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2
                   dtext = _fminf(dtext, _fmaxf(max(dbox.x, dbox.y), grad_dist.z));
                 }
 
-
-
-
-
                 nonzero = true;
-
             }
-
         }
 
         float3 textcolor = ktex[2] ? to_float3_s(0.0f) : (ktex[1] || ktex[0]) ? to_float3_s(1.0f) : to_float3_s(0.9f);
@@ -114,17 +103,13 @@ __KERNEL__ void KeyboardDebuggingFuse(float4 fragColor, float2 fragCoord, float2
         color = _mix(color, textcolor, smoothstep(0.5f*scl, -0.5f*scl, dtext));
 
         float2 p0 = _floor(p/CELL_DIMS + 0.5f)*CELL_DIMS;
-        float2 dp0 = _fabs(p - p0);
+        float2 dp0 = abs_f2(p - p0);
         dbox = _fminf(_fabs(dbox), _fminf(_fabs(dp0.x), _fabs(dp0.y)));
-
     }
 
     color *= smoothstep(0.0f, scl, _fabs(dbox));
 
     fragColor = to_float4_aw(color, 1);
-
-
-
 
   SetFragmentShaderComputedColor(fragColor);
 }
