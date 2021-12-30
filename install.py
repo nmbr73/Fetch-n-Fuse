87e46ad9636a4710378a5e8ba93eb99e3aeb149d
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2021-12-30 21:17:18 +0100

    Adds repo path.

--- install.py
+++ install.py
@@ -8,15 +8,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 fusePath=os.getenv('FUSEPATH')
-repoPath="./"
+repoPath=os.getenv('REPOPATH')
 
-if not fusePath:
-  raise Exception("make sure run a 'fetch' first to have the '.env' file created. there set FUSEPATH accordingly.")
+if not fusePath or not repoPath:
+  print("make sure run a 'fetch' first to have the '.env' file created.\n then set FUSEPATH and REPOPATH accordingly.\nmake sure to have the paths end with trailing slashes.")
+else:
 
-with open(repoPath+".clobber/Incubator.fuse", "r") as f:
-  incubatorFuse=f.read()
+  with open(repoPath+".clobber/Incubator.fuse", "r") as f:
+    incubatorFuse=f.read()
 
-incubatorFuse=re.sub(r'\s+local\ss+REPOSITORYPATH\s*=\s*\[\[[^\]]*\]\]', '\n\n  local REPOSITORYPATH=[['+repoPath+']]',incubatorFuse)
+  incubatorFuse=re.sub(r'\s+local\s+REPOSITORYPATH\s*=\s*\[\[[^\]]*\]\]', '\n\n  local REPOSITORYPATH=[['+repoPath+']]',incubatorFuse)
 
-with open(fusePath+"Incubator_temp.fuse", "w") as f:
-  f.write(incubatorFuse)
+  with open(fusePath+"Incubator_temp.fuse", "w") as f:
+    f.write(incubatorFuse)
