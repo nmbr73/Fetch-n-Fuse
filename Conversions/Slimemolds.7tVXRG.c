ebb84cb7aa7a381d3eb53918be227ef9030f2e2d
J-i-P-i <78935215+J-i-P-i@users.noreply.github.com>
2022-01-01 13:22:16 +0100

    Slimemolds & TruchetKaleidoscope

---
+++ Conversions/Slimemolds.7tVXRG.c
@@ -0,0 +1,292 @@
+
+// ----------------------------------------------------------------------------------
+// - Common                                                                         -
+// ----------------------------------------------------------------------------------
+
+__DEVICE__ float2 sin_f2(float2 i) {float2 r; r.x = _sinf(i.x); r.y = _sinf(i.y); return r;}
+
+
+#define NB      10000.
+
+#define SENSE_ANGLE    15.0f
+#define SENSE_DIST      5.0f
+#define SENSE_SIZE      3.0f
+
+#define SPEED 2.5
+
+#define SCENT_INTENSITY  0.9f
+#define SCENT_DECAY      0.001f
+#define SCENT_DESPERSION 0.1f
+
+#define SCREEN_X      450.0f
+#define SCREEN_Y      450.0f
+
+#define EXPLORE 0.001
+
+__DEVICE__ float getId(float2 resolution, float2 fragCoord) { 
+    return _floor(fragCoord.x*resolution.y + fragCoord.y);
+    //return _floor(fragCoord.x);
+}
+
+// ----------------------------------------------------------------------------------
+// - Buffer A                                                                       -
+// ----------------------------------------------------------------------------------
+// Connect 'Buffer A' to iChannel0
+// Connect 'Buffer B' to iChannel1
+
+
+//Cell management
+
+__DEVICE__ float2 hash(float n) { 
+    return fract_f2(sin_f2(to_float2(n,n*7.0f))* 43758.5453f); 
+}
+
+__DEVICE__ float4 ParticleA(float2 fragCoord, float2 iResolution, __TEXTURE2D__ iChannel0) { 
+    //return texelFetch(iChannel0, to_int2(fragCoord),0);
+    return _tex2DVecN(iChannel0, (fragCoord.x)/iResolution.x,(fragCoord.y)/iResolution.y,15); //interessante Vaiante mit 0.5f
+}
+
+__DEVICE__ float sense(float2 pos, float size, float2 iResolution, __TEXTURE2D__ iChannel1){
+    float total = 0.0f;
+    for(float x = -size/2.0f; x <= size/2.0f; x++){
+        for(float y = -size/2.0f; y <= size/2.0f; y++) {
+            float2 sense_loc = pos + to_float2(x , y);
+            
+            //total += length(texelFetch(iChannel1, to_int2(sense_loc),0));
+            total += length(_tex2DVecN(iChannel1, (sense_loc.x+0.0f)/iResolution.x, (sense_loc.y+0.0f)/iResolution.y,15)); //interessante Vaiante
+            
+            //total += length(texture(iChannel1, (to_float2(to_int2(sense_loc))+0.5f)/iResolution));
+            
+        }
+    }
+
+    return total;
+}
+
+__DEVICE__ float4 changeDirection(float4 fish, float2 iResolution, __TEXTURE2D__ iChannel1) {
+    float sense_angle = radians(SENSE_ANGLE);
+    float dist = SENSE_DIST;
+    float size = SENSE_SIZE;
+    
+    float angle = _atan2f(fish.w, fish.z);
+    float new_angle = 0.0f;
+    
+    float sense_angle1 = angle + sense_angle;
+    float2 sense_pixel1 = swi2(fish,x,y) + to_float2(_cosf(sense_angle1)*dist,_sinf(sense_angle1)*dist);
+    float sense_value1 = sense(sense_pixel1,size,iResolution,iChannel1);
+    
+    float sense_angle2 = angle;
+    float2 sense_pixel2 = swi2(fish,x,y) + to_float2(_cosf(sense_angle2)*dist,_sinf(sense_angle2)*dist);
+    float sense_value2 = sense(sense_pixel2,size,iResolution,iChannel1);
+    
+    float sense_angle3 = angle - sense_angle;
+    float2 sense_pixel3 = swi2(fish,x,y) + to_float2(_cosf(sense_angle3)*dist,_sinf(sense_angle3)*dist);
+    float sense_value3 = sense(sense_pixel3,size,iResolution,iChannel1);
+    
+    float explore = fract(_sinf(dot(swi2(fish,z,w), swi2(fish,x,y))) * 43758.43758f);
+    if(explore < EXPLORE){
+        float rand = fract(_sinf(dot(swi2(fish,z,w), swi2(fish,x,y))) * 5453.5453f);
+        if(rand > 0.5f) 
+            new_angle = sense_angle1;
+        else
+            new_angle = sense_angle3;
+    } 
+    else if(sense_value1 > sense_value2 && sense_value1 > sense_value3) {
+        new_angle = sense_angle1;
+    } 
+    
+    //else if(sense_value2 > sense_value1 && sense_value2 > sense_value3){
+    //    new_angle = sense_angle2  ;   
+    //}
+    
+    else if(sense_value3 > sense_value2 && sense_value3 > sense_value1) {
+        new_angle = sense_angle3 ;    
+    }
+    
+    else {
+        float rand = fract(_sinf(dot(swi2(fish,z,w), swi2(fish,x,y))) * 43758.5453f);
+        if(rand > 0.5f) 
+            new_angle = sense_angle1;
+        else
+            new_angle = sense_angle3;
+    }
+
+    //swi2(fish,z,w) = to_float2(_cosf(new_angle)*SPEED, _sinf(new_angle)*SPEED);
+    fish.w = _cosf(new_angle)*SPEED;
+    fish.w = _sinf(new_angle)*SPEED;
+    
+    return fish;
+    
+}
+
+
+__KERNEL__ void SlimemoldsFuse__Buffer_A(float4 fragColor, float2 fragCoord, float2 iResolution, int iFrame, sampler2D iChannel0, sampler2D iChannel1)
+{
+
+    
+    float2 screensize = iResolution;
+    //vec2 screensize = to_float2(SCREEN_X,SCREEN_Y);
+    
+    float4 col = to_float4_s(0);
+    
+    if(iFrame < 10){
+        float id = getId(screensize,fragCoord);
+        float2 random_pos = hash(id)*screensize;
+        float angle = radians(hash(id).x*hash(id).y*360.0f);
+        
+        float2 random_direction = to_float2(_cosf(angle)*SPEED,_sinf(angle)*SPEED);
+        fragColor = to_float4(fragCoord.x,fragCoord.y,random_direction.x,random_direction.y);
+    }
+    else {
+        //col = to_float4(uv.x,0.0f,0.0f,0.1f);
+        //vec4 fish1 = Fish(0)
+        float4 fish = ParticleA(fragCoord,iResolution,iChannel0);
+        fish = changeDirection(fish,iResolution,iChannel1);
+
+        
+        //swi2(fish,x,y) += swi2(fish,z,w);
+        fish.x += fish.z;
+        fish.y += fish.w;
+        
+        //Edge detection
+        if(fish.x < 0.0f){
+            fish.x = 0.0f;
+            fish.z *= -1.0f;
+        }
+        if(fish.y < 0.0f){
+            fish.y = 0.0f;
+            fish.w *= -1.0f;
+        }
+        if(fish.x > screensize.x){
+            fish.x = screensize.x;
+            fish.z *= -1.0f;
+        }
+        if(fish.y > screensize.y){
+            fish.y = screensize.y;
+            fish.w *= -1.0f;
+        }
+        
+        fragColor = fish;
+    }
+    
+
+
+  SetFragmentShaderComputedColor(fragColor);
+}
+// ----------------------------------------------------------------------------------
+// - Buffer B                                                                       -
+// ----------------------------------------------------------------------------------
+// Connect 'Buffer A' to iChannel0
+// Connect 'Buffer B' to iChannel1
+
+
+//Scent
+__DEVICE__ float4 ParticleB(float2 fragCoord, float2 iResolution, __TEXTURE2D__ iChannel0) { 
+    //return texelFetch(iChannel0, to_int2(fragCoord),0);
+    //return texture(iChannel0, (fragCoord+0.5f)/iResolution);
+    return _tex2DVecN(iChannel0, (fragCoord.x+0.5f)/iResolution.x,(fragCoord.y+0.5f)/iResolution.y,15); //interessante Vaiante mit 0.5f
+}
+__DEVICE__ float drawScent(float2 uv, float2 coord) {
+    float d = length(to_float2(uv.x - coord.x, uv.y - coord.y));
+    //return 0.03f/d;
+    return 0.3f/d < 0.2f ? 0.0f : SCENT_INTENSITY;
+}
+
+__DEVICE__ float removeScent(float2 uv, float2 coord) {
+    float d = length(to_float2(uv.x - coord.x, uv.y - coord.y));
+    //return 0.03f/d;
+    return 0.3f/d < 0.5f ? 0.0f : SCENT_INTENSITY;
+}
+
+
+__KERNEL__ void SlimemoldsFuse__Buffer_B(float4 fragColor, float2 fragCoord, float2 iResolution, float4 iMouse, int iFrame, float3 iChannelResolution[], sampler2D iChannel0, sampler2D iChannel1)
+{
+
+    float2 screensize = iResolution;
+
+    float4 col = to_float4_s(0.0f);
+    float2 uv = fragCoord;
+    float id = getId(screensize,fragCoord);
+    
+    // = Initialization ===================================
+    if(iFrame > 10){
+
+        //col = _tex2DVecN(iChannel1,fragCoord.x,fragCoord.y,15);
+        //col = texelFetch(iChannel1, to_int2(fragCoord),0)*0.995f;
+        //col = texelFetch(iChannel1, to_int2(fragCoord),0);
+        
+        float blur = SCENT_DESPERSION;
+
+        for(int i = -1;i<2;i++)
+            for(int j = -1;j<2;j++)
+                //col += _tex2DVecN(iChannel1,to_float2(fragCoord/iResolution)+to_float2(i,j)/iChannelResolution[1].xy*blur*1.0f,blur);
+                col += _tex2DVecN(iChannel1,(fragCoord.x/iResolution.x)+(i)/iResolution.x*blur*1.0f,(fragCoord.y/iResolution.y)+(j)/iResolution.y*blur*1.0f,15);
+              
+        col/=9.05f;
+        
+        col-=SCENT_DECAY;
+        
+        
+ 
+        for(float i = 0.0f;i<=400.0f;i+=1.0f){
+            for(float t = 0.0f;t<=15.0f;t+=1.0f){
+                float4 particle = ParticleB(to_float2(i,t),iResolution,iChannel0);
+                col += drawScent(fragCoord, swi2(particle,x,y));
+            }
+        }
+    
+    }
+    
+    fragColor = _fminf(col,to_float4_s(1.0f));
+    
+    //fragColor = 0.3f/length(to_float2(fragCoord-swi2(iMouse,x,y))) > 0.01f ? to_float4_s(0.0f) : col ;  
+    
+    
+
+
+  SetFragmentShaderComputedColor(fragColor);
+}
+// ----------------------------------------------------------------------------------
+// - Image                                                                          -
+// ----------------------------------------------------------------------------------
+// Connect 'Buffer A' to iChannel0
+// Connect 'Buffer B' to iChannel1
+
+
+__DEVICE__ float4 ParticleI(float2 fragCoord, float2 iResolution, __TEXTURE2D__ iChannel0) { 
+    //return texelFetch(iChannel0, to_int2(fragCoord),0);
+    //return texture(iChannel0, (fragCoord+0.5f)/iResolution);
+    return _tex2DVecN(iChannel0, (fragCoord.x)/iResolution.x,(fragCoord.y)/iResolution.y,15); //interessante Vaiante mit 0.5f
+}
+
+__DEVICE__ float drawParticle(float2 uv, float2 coord) {
+    float d = length(to_float2(uv.x - coord.x, uv.y - coord.y));
+    return 0.5f/d < 0.5f ? 0.0f : 1.0f;
+}
+
+__KERNEL__ void SlimemoldsFuse(float4 fragColor, float2 fragCoord, float2 iResolution, sampler2D iChannel0, sampler2D iChannel1)
+{
+
+    
+    //vec2 screensize = to_float2(SCREEN_X,SCREEN_Y);
+    float2 screensize = iResolution;
+    float2 uv = fragCoord;
+   
+    float4 col = to_float4_s(0);
+    
+    for(float i = 0.0f;i<=400.0f;i+=1.0f){
+        for(float t = 0.0f;t<=15.0f;t+=1.0f){
+            float4 particle = ParticleI(to_float2(i,t),iResolution,iChannel0);
+           //vec4 fish1 = Fish(fragCoord);
+            col += drawParticle(uv, swi2(particle,x,y));
+        }
+    }
+    
+    //col += texelFetch(iChannel1, to_int2(uv),0);
+    col += _tex2DVecN(iChannel1, (uv.x+0.5f)/iResolution.x,(uv.y+0.5f)/iResolution.y,15); //Interessante Variante ohne 0.5
+    // Output to screen
+    fragColor = col;
+
+
+  SetFragmentShaderComputedColor(fragColor);
+}
---
+++ Conversions/TruchetKaleidoscopeFtw.7lKSWW.c
@@ -0,0 +1,316 @@
+
+// ----------------------------------------------------------------------------------
+// - Image                                                                          -
+// ----------------------------------------------------------------------------------
+
+__DEVICE__ float2 abs_f2(float2 a) {return (to_float2(_fabs(a.x), _fabs(a.y)));}
+__DEVICE__ float3 pow_f3(float3 a, float3 b) {float3 r; r.x = _powf(a.x,b.x); r.y = _powf(a.y,b.y); r.z = _powf(a.z,b.z); return r;}
+__DEVICE__ float sign_f(float value) { if (value == 0.0f) return 0.0f; return value > 0.0f ? 1.0f : -1.0f;}
+
+// CC0: Truchet + Kaleidoscope FTW
+//  Bit of experimenting with kaleidoscopes and truchet turned out nice
+//  Quite similar to an earlier shader I did but I utilized a different truchet pattern this time
+#define PI              3.141592654
+#define TAU             (2.0f*PI)
+#define RESOLUTION      iResolution
+#define TIME            iTime
+#define ROT(a)          to_mat2(_cosf(a), _sinf(a), -_sinf(a), _cosf(a))
+#define PCOS(x)         (0.5f+0.5f*_cosf(x))
+
+// License: Unknown, author: Unknown, found: don't remember
+__DEVICE__ float4 alphaBlend(float4 back, float4 front) {
+  float w = front.w + back.w*(1.0f-front.w);
+  float3 xyz = (swi3(front,x,y,z)*front.w + swi3(back,x,y,z)*back.w*(1.0f-front.w))/w;
+  return w > 0.0f ? to_float4_aw(xyz, w) : to_float4_s(0.0f);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+__DEVICE__ float3 alphaBlend(float3 back, float4 front) {
+  return _mix(back, swi3(front,x,y,z), front.w);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+__DEVICE__ float hash(float co) {
+  return fract(_sinf(co*12.9898f) * 13758.5453f);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+__DEVICE__ float hash(float2 p) {
+  float a = dot(p, to_float2 (127.1f, 311.7f));
+  return fract(_sinf (a)*43758.5453123f);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+__DEVICE__ float tanh_approx(float x) {
+  //  Found this somewhere on the interwebs
+  //  return _tanhf(x);
+  float x2 = x*x;
+  return clamp(x*(27.0f + x2)/(27.0f+9.0f*x2), -1.0f, 1.0f);
+}
+
+// License: MIT, author: Inigo Quilez, found: https://www.iquilezles.org/www/articles/smin/smin.htm
+__DEVICE__ float pmin(float a, float b, float k) {
+  float h = clamp(0.5f+0.5f*(b-a)/k, 0.0f, 1.0f);
+  return _mix(b, a, h) - k*h*(1.0f-h);
+}
+
+// License: MIT, author: Inigo Quilez, found: https://www.iquilezles.org/www/index.htm
+__DEVICE__ float3 postProcess(float3 col, float2 q) {
+  col = clamp(col, 0.0f, 1.0f);
+  col = pow_f3(col, to_float3_s(1.0f/2.2f));
+  col = col*0.6f+0.4f*col*col*(3.0f-2.0f*col);
+  col = _mix(col, to_float3_s(dot(col, to_float3_s(0.33f))), -0.4f);
+  col *=0.5f+0.5f*_powf(19.0f*q.x*q.y*(1.0f-q.x)*(1.0f-q.y),0.7f);
+  return col;
+}
+
+__DEVICE__ float pmax(float a, float b, float k) {
+  return -pmin(-a, -b, k);
+}
+
+__DEVICE__ float pabs(float a, float k) {
+  return pmax(a, -a, k);
+}
+
+__DEVICE__ float2 toPolar(float2 p) {
+  return to_float2(length(p), _atan2f(p.y, p.x));
+}
+
+__DEVICE__ float2 toRect(float2 p) {
+  return to_float2(p.x*_cosf(p.y), p.x*_sinf(p.y));
+}
+
+// License: MIT OR CC-BY-NC-4.0f, author: mercury, found: https://mercury.sexy/hg_sdf/
+__DEVICE__ float modMirror1(inout float *p, float size) {
+  float halfsize = size*0.5f;
+  float c = _floor((*p + halfsize)/size);
+  *p = mod_f(*p + halfsize,size) - halfsize;
+  *p *= mod_f(c, 2.0f)*2.0f - 1.0f;
+  return c;
+}
+
+__DEVICE__ float smoothKaleidoscope(inout float2 *p, float sm, float rep) {
+  float2 hp = *p;
+
+  float2 hpp = toPolar(hp);
+  float rn = modMirror1(&hpp.y, TAU/rep);
+
+  float sa = PI/rep - pabs(PI/rep - _fabs(hpp.y), sm);
+  hpp.y = sign_f(hpp.y)*(sa);
+
+  hp = toRect(hpp);
+
+  *p = hp;
+
+  return rn;
+}
+
+// The path function
+__DEVICE__ float3 offset(float z) {
+  float a = z;
+  float2 p = -0.075f*(to_float2(_cosf(a), _sinf(a*_sqrtf(2.0f))) + to_float2(_cosf(a*_sqrtf(0.75f)), _sinf(a*_sqrtf(0.5f))));
+  return to_float3_aw(p, z);
+}
+
+// The derivate of the path function
+//  Used to generate where we are looking
+__DEVICE__ float3 doffset(float z) {
+  float eps = 0.1f;
+  return 0.5f*(offset(z + eps) - offset(z - eps))/eps;
+}
+
+// The second derivate of the path function
+//  Used to generate tilt
+__DEVICE__ float3 ddoffset(float z) {
+  float eps = 0.1f;
+  return 0.125f*(doffset(z + eps) - doffset(z - eps))/eps;
+}
+
+__DEVICE__ float2 cell_df(float r, float2 np, float2 mp, float2 off) {
+  const float2 n0 = normalize(to_float2(1.0f, 1.0f));
+  const float2 n1 = normalize(to_float2(1.0f, -1.0f));
+
+  np += off;
+  mp -= off;
+  
+  float hh = hash(np);
+  float h0 = hh;
+
+  float2  p0 = mp;  
+  p0 = abs_f2(p0);
+  p0 -= 0.5f;
+  float d0 = length(p0);
+  float d1 = _fabs(d0-r); 
+
+  float dot0 = dot(n0, mp);
+  float dot1 = dot(n1, mp);
+
+  float d2 = _fabs(dot0);
+  float t2 = dot1;
+  d2 = _fabs(t2) > _sqrtf(0.5f) ? d0 : d2;
+
+  float d3 = _fabs(dot1);
+  float t3 = dot0;
+  d3 = _fabs(t3) > _sqrtf(0.5f) ? d0 : d3;
+
+
+  float d = d0;
+  d = _fminf(d, d1);
+  if (h0 > 0.85f)
+  {
+    d = _fminf(d, d2);
+    d = _fminf(d, d3);
+  }
+  else if(h0 > 0.5f)
+  {
+    d = _fminf(d, d2);
+  }
+  else if(h0 > 0.15f)
+  {
+    d = _fminf(d, d3);
+  }
+  
+  return to_float2(d, d0-r);
+}
+
+__DEVICE__ float2 truchet_df(float r, float2 p) {
+  float2 np = _floor(p+0.5f);
+  float2 mp = fract_f2(p+0.5f) - 0.5f;
+  return cell_df(r, np, mp, to_float2_s(0.0f));
+}
+
+__DEVICE__ float4 plane(float3 ro, float3 rd, float3 pp, float3 off, float aa, float n,float iTime) {
+  float h_ = hash(n);
+  float h0 = fract_f(1777.0f*h_);
+  float h1 = fract_f(2087.0f*h_);
+  float h2 = fract_f(2687.0f*h_);
+  float h3 = fract_f(3167.0f*h_);
+  float h4 = fract_f(3499.0f*h_);
+
+  float l = length(pp - ro);
+
+  float3 hn;
+  float2 p = swi2((pp-off*to_float3(1.0f, 1.0f, 0.0f)),x,y);
+  p = mul_f2_mat2(p,ROT(0.5f*(h4 - 0.5f)*TIME));
+  float rep = 2.0f*_round(_mix(5.0f, 30.0f, h2));
+  float sm = 0.05f*20.0f/rep;
+  float sn = smoothKaleidoscope(&p, sm, rep);
+  p = mul_f2_mat2(p,ROT(TAU*h0+0.025f*TIME));
+  float z = _mix(0.2f, 0.4f, h3);
+  p /= z;
+  p+=0.5f+_floor(h1*1000.0f);
+  float tl = tanh_approx(0.33f*l);
+  float r = _mix(0.30f, 0.45f, PCOS(0.1f*n));
+  float2 d2 = truchet_df(r, p);
+  d2 *= z;
+  float d = d2.x;
+  float lw =0.025f*z; 
+  d -= lw;
+  
+  float3 col = _mix(to_float3_s(1.0f), to_float3_s(0.0f), smoothstep(aa, -aa, d));
+  col = _mix(col, to_float3_s(0.0f), smoothstep(_mix(1.0f, -0.5f, tl), 1.0f, _sinf(PI*100.0f*d)));
+//  float t0 = smoothstep(aa, -aa, -d2.y-lw);
+  col = _mix(col, to_float3_s(0.0f), step(d2.y, 0.0f));
+  //float t = smoothstep(3.0f*lw, 0.0f, -d2.y);
+//  float t = smoothstep(aa, -aa, -d2.y-lw);
+  float t = smoothstep(aa, -aa, -d2.y-3.0f*lw)*_mix(0.5f, 1.0f, smoothstep(aa, -aa, -d2.y-lw));
+  return to_float4_aw(col, t);
+}
+
+__DEVICE__ float3 skyColor(float3 ro, float3 rd) {
+  float d = _powf(_fmaxf(dot(rd, to_float3(0.0f, 0.0f, 1.0f)), 0.0f), 20.0f);
+  return to_float3_s(d);
+}
+
+__DEVICE__ float3 color(float3 ww, float3 uu, float3 vv, float3 ro, float2 p, float iTime, float2 iResolution) {
+
+  float lp = length(p);
+  float2 np = p + 1.0f/RESOLUTION;
+  float rdd = (2.0f+1.0f*tanh_approx(lp));
+//  float rdd = 2.0f;
+  float3 rd = normalize(p.x*uu + p.y*vv + rdd*ww);
+  float3 nrd = normalize(np.x*uu + np.y*vv + rdd*ww);
+
+  const float planeDist = 1.0f-0.25f;
+  const int furthest = 6;
+  const int fadeFrom = _fmaxf(furthest-5, 0);
+
+  const float fadeDist = planeDist*(float)(furthest - fadeFrom);
+  float nz = _floor(ro.z / planeDist);
+
+  float3 skyCol = skyColor(ro, rd);
+
+
+  float4 acol = to_float4_s(0.0f);
+  const float cutOff = 0.95f;
+  bool cutOut = false;
+
+  // Steps from nearest to furthest plane and accumulates the color 
+  for (int i = 1; i <= furthest; ++i) {
+    float pz = planeDist*nz + planeDist*(float)(i);
+
+    float pd = (pz - ro.z)/rd.z;
+
+    if (pd > 0.0f && acol.w < cutOff) {
+      float3 pp = ro + rd*pd;
+      float3 npp = ro + nrd*pd;
+
+      float aa = 3.0f*length(pp - npp);
+
+      float3 off = offset(pp.z);
+
+      float4 pcol = plane(ro, rd, pp, off, aa, nz+(float)(i),iTime);
+
+      float nz = pp.z-ro.z;
+      float fadeIn = smoothstep(planeDist*(float)(furthest), planeDist*(float)(fadeFrom), nz);
+      float fadeOut = smoothstep(0.0f, planeDist*0.1f, nz);
+      pcol = to_float4_aw(_mix(skyCol, swi3(pcol,x,y,z), fadeIn),pcol.w);
+  
+      pcol.w *= fadeOut;
+      pcol = clamp(pcol, 0.0f, 1.0f);
+
+      acol = alphaBlend(pcol, acol);
+    } else {
+      cutOut = true;
+      break;
+    }
+
+  }
+
+  float3 col = alphaBlend(skyCol, acol);
+  // To debug cutouts due to transparency  
+  //  col += cutOut ? to_float3(1.0f, -1.0f, 0.0f) : to_float3_s(0.0f);
+  return col;
+}
+
+__DEVICE__ float3 effect(float2 p, float2 q, float iTime, float2 iResolution) {
+  float tm  = TIME*0.25f;
+  float3 ro   = offset(tm);
+  float3 dro  = doffset(tm);
+  float3 ddro = ddoffset(tm);
+
+  float3 ww = normalize(dro);
+  float3 uu = normalize(cross(normalize(to_float3(0.0f,1.0f,0.0f)+ddro), ww));
+  float3 vv = normalize(cross(ww, uu));
+
+  float3 col = color(ww, uu, vv, ro, p,iTime,iResolution);
+  
+  return col;
+}
+
+__KERNEL__ void TruchetKaleidoscopeFtwFuse(float4 fragColor, float2 fragCoord, float iTime, float2 iResolution)
+{
+
+  float2 q = fragCoord/RESOLUTION;
+  float2 p = -1.0f + 2.0f * q;
+  p.x *= RESOLUTION.x/RESOLUTION.y;
+  
+  float3 col = effect(p, q,iTime,iResolution);
+  col *= smoothstep(0.0f, 4.0f, TIME);
+  col = postProcess(col, q);
+ 
+  fragColor = to_float4_aw(col, 1.0f);
+
+
+  SetFragmentShaderComputedColor(fragColor);
+}
