4e5cd4834ab396f72bb5afe164e78355fcfd4271
J-i-P-i <78935215+J-i-P-i@users.noreply.github.com>
2022-01-03 22:04:55 +0100

    Checkboxen & Pointcontrols hinzugefügt (Testshader: SimpleJiPi.TEST)

--- Conversions/SimpleJiPi.TEST.c
+++ Conversions/SimpleJiPi.TEST.c
@@ -1,17 +1,27 @@
 
 
-__KERNEL__ void SimpleJiPiFuse(float4 fragColor)
+__KERNEL__ void SimpleJiPiFuse(float4 fragColor, float2 fragCoord, float2 iResolution)
 {
   CONNECT_COLOR0(JiPi_Color, 0.290196078431373f, 0.254901960784314f, 0.164705882352941f, 1.0f);
   CONNECT_COLOR3(AnotherFunnyColor, 0.9f, 0.2f, 0.1f, 1.0f);
-  CONNECT_TINYSLIDER0(JiPiSlider, 0.0f, 10.0f, 5.0f);
+  CONNECT_TINYSLIDER0(JiPiSlider, 0.0f, 300.0f, 200.0f);
   CONNECT_SMALLSLIDER0(SmallJiPi, -1.5f, 1.5f, -0.5f);
   CONNECT_TINYINT0(IntegerEinfach, -5, 5, 2);
+  CONNECT_CHECKBOX2(BlackisBeautiful, 0);
+  CONNECT_CHECKBOX3(CheckThis, 0);
+  CONNECT_POINT0(Look, 0.8f, 0.35f);
+  
 
-  float red          = JiPi_Color.x*AnotherFunnyColor.x*JiPiSlider;
+
+  float red          = JiPi_Color.x*AnotherFunnyColor.x;
   float green        = JiPi_Color.y*AnotherFunnyColor.y;
   float blue         = JiPi_Color.z*AnotherFunnyColor.z;
   float alpha        = JiPi_Color.w*AnotherFunnyColor.w;
+
+  if (length(fragCoord-(Look*iResolution))<JiPiSlider && CheckThis)  red=1.0f,green=0.0,blue=1.0f,alpha=1.0f;
+
+  if(BlackisBeautiful) red=0.0f,green=0.0,blue=0.0f,alpha=0.5f;
+
 
   fragColor=to_float4(red,green,blue,alpha);
 
