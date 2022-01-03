37a689108874ad449912da0a64ea26af3a8456f8
J-i-P-i <78935215+J-i-P-i@users.noreply.github.com>
2022-01-03 13:42:45 +0100

    Reload Parameter in Incubatorfuse

--- Conversions/SimpleJiPi.TEST.c
+++ Conversions/SimpleJiPi.TEST.c
@@ -2,12 +2,16 @@
 
 __KERNEL__ void SimpleJiPiFuse(float4 fragColor)
 {
-  CONNECT_COLOR0(color, 0.290196078431373f, 0.254901960784314f, 0.164705882352941f, 1.0f);
+  CONNECT_COLOR0(JiPi_Color, 0.290196078431373f, 0.254901960784314f, 0.164705882352941f, 1.0f);
+  CONNECT_COLOR3(AnotherFunnyColor, 0.9f, 0.2f, 0.1f, 1.0f);
+  CONNECT_TINYSLIDER0(JiPiSlider, 0.0f, 10.0f, 5.0f);
+  CONNECT_SMALLSLIDER0(SmallJiPi, -1.5f, 1.5f, -0.5f);
+  CONNECT_TINYINT0(IntegerEinfach, -5, 5, 2);
 
-  float red          = color.x;
-  float green        = color.y;
-  float blue         = color.z;
-  float alpha        = color.w;
+  float red          = JiPi_Color.x*AnotherFunnyColor.x*JiPiSlider;
+  float green        = JiPi_Color.y*AnotherFunnyColor.y;
+  float blue         = JiPi_Color.z*AnotherFunnyColor.z;
+  float alpha        = JiPi_Color.w*AnotherFunnyColor.w;
 
   fragColor=to_float4(red,green,blue,alpha);
 
